7487d42d996a554a7ef9d53e56c63226c808c670
Irum F <irummf@gmail.com>
2018-10-14 17:23:52 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,10 +18,10 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params["post"])
-	  redirect_to post_path(@post)
-	end
+    @post = Post.find(params[:id])
+    @post.update(params.require(:post).permit(:title))
+    redirect_to post_path(@post)
+  end
 
 	def edit
 	  @post = Post.find(params[:id])
